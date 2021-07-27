ba2fc15daf05a28703d1b3e3e830a0b2e2e8c6f9
stuge <tuukkaveteli@gmail.com>
2021-07-27 18:32:46 +0300

    setting up config files

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'@typescript-eslint/semi': ['error'],
+		'@typescript-eslint/explicit-function-return-type': 0,
+		'@typescript-eslint/explicit-module-boundary-types': 0,
+		'@typescript-eslint/no-unsafe-member-access': 0,
+		'@typescript-eslint/no-explicit-any': 1,
+		'@typescript-eslint/no-unused-vars': [
+			'error', { 'argsIgnorePattern': '^_' }
+		],
+		'no-case-declarations': 0,
+		'react/prop-types': 0
+	}
+};
