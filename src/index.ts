d9326726cf02229d74924e85361b1e1439f1a19f
stuge <tuukkaveteli@gmail.com>
2021-07-27 19:33:27 +0300

    fixed server start up

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import http from 'http';
 import { PORT } from './config';
 import app from './app';
 
-const server = http.createServer(app.callback()).listen(PORT);
+
+const server = http.createServer(app).listen(PORT);
 
 server.on('listening', () => {
     console.info(`Server listening to port ${PORT}`);
