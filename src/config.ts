c7eeefba0d56a69388f5285185f4d3a73f687e96
stuge <tuukkaveteli@gmail.com>
2021-07-27 19:13:59 +0300

    exports have types

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
+
+export const PORT: number = process.env.PORT
+	? parseInt(process.env.PORT)
+	: 3001;
+
+export const GITHUB_API_URL: string =
+  process.env.GITHUB_API_URL || 'https://api.github.com';
+
+export const GITHUB_CLIENT_ID: string | undefined  = process.env.GITHUB_CLIENT_ID;
+
+export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
