9c8772e6942a8f046539abafb91cffb6e62e4fd6
stuge <tuukkaveteli@gmail.com>
2021-07-28 00:58:58 +0300

    broke up the data processing into smaller funcs

--- src/utils/migrate.ts
+++ src/utils/migrate.ts
@@ -1,53 +1,86 @@
 // import VaccineOrder from '../models/order';
 // import Vaccination from '../models/vaccination';
-import { /* Vaccination as Vax, */ VaccineOrder as Order } from '../types'
+import { Vaccination as Vax, VaccineOrder as Order } from '../types'
 import fs from 'fs';
 
 require('express-async-errors');
 
-const orderFiles = [
-	'/../data/Antiqua.source',
-	'/../data/SolarBuddhica.source',
-	'/../data/Zerpfy.source',
-];
+interface rawOrder extends Order {
+	id: string
+}
 
-const allOrders: Order[] = []
+const parseOrder = (rawObject: rawOrder): Order => (
+	{
+		orderId: rawObject.id,
+		healthCareDistrict: rawObject.healthCareDistrict,
+		orderNumber: rawObject.orderNumber,
+		responsiblePerson: rawObject.responsiblePerson,
+		injections: rawObject.injections,
+		arrived: rawObject.arrived,
+		vaccine: rawObject.vaccine,
+	}
+)
 
-orderFiles.forEach((filename) => {
-	fs.readFile(__dirname + filename, 'utf-8', (error, data) => {
-		console.log('formatting:', filename)
-		if (error) { throw error; }
 
-		// format the data to a json string
-		const orderObjects: Order[] = data
-			.slice(0, -1)
-			.split('\n')
-			.map((line) => {
-				try {
-					const rawObject = JSON.parse(line)
-					return {
-						orderId: rawObject.id,
-						healthCareDistrict: rawObject.healthCareDistrict,
-						orderNumber: rawObject.orderNumber,
-						responsiblePerson: rawObject.responsiblePerson,
-						injections: rawObject.injections,
-						arrived: rawObject.arrived,
-						vaccine: rawObject.vaccine,
-					}
-				} catch (e) {
-					throw new Error(e)
-				}
-			})
-		allOrders.push(orderObjects[0])
-		console.log(...orderObjects)
+interface rawVax extends Vax {
+	id: string
+}
+
+const parseVaccination = (rawObject: rawVax): Vax => (
+	{
+		vaccinationId: rawObject.id,
+		gender: rawObject.gender,
+		sourceBottle: rawObject.sourceBottle,
+		injected: rawObject.injected,
+	}
+)
+
+/**
+ * returns an array of json objects
+ * @param data  -String where each new line is a JSON object
+ *  */
+const stringToObjectArray = (data: string): Order[] | Vax[] => (
+	data
+		.slice(0, -1) // remove trailing new line from EoF
+		.split('\n')  // make an array of lines
+		.map((line) => {
+			// turn line into an object
+			const rawObject = JSON.parse(line)
+			if (rawObject.orderNumber) {
+				return parseOrder(rawObject)
+			}
+			if (rawObject.gender && rawObject.sourceBottle) {
+				return parseVaccination(rawObject)
+			}
+			throw new Error('Invalid object: \n' + line)
+		})
+)
+
+const getDataFromFiles = (sources: string[]): Order[] | Vax[] => {
+	return sources.map((source): (Order | Vax) => {
+		let dataString = ''
+		fs.readFile(__dirname + source, 'utf-8', (error, data) => {
+			console.log('formatting:', source);
+			if (error) throw error
+			dataString = data;
+		});
+		return stringToObjectArray(dataString);
 	});
-	console.log(allOrders)
-});
-//
-// const populateOrders = () => {
+}
 
-// };
+const migration = () => {
+	const orderFiles = [
+		'/../data/Antiqua.source',
+		'/../data/SolarBuddhica.source',
+		'/../data/Zerpfy.source',
+	];
+	const vaccinationFiles = [
+		'/../data/vaccinations.source'
+	]
 
-// const populateVaccinations = () => {
+	const orders: Order[] = getDataFromFiles(orderFiles)
+	const vaccinations: Vax[] = getDataFromFiles(vaccinationFiles)
+	console.log(orders)
+}
 
-// };
+migration();
