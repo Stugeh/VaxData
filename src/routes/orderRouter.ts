9289902fbb1edbd3cb405d4752d138cc3ba7021f
stuge <tuukkaveteli@gmail.com>
2021-07-28 17:07:00 +0300

    added a function to sort orders by producer

--- src/routes/orderRouter.ts
+++ src/routes/orderRouter.ts
@@ -1,15 +1,37 @@
-import express from 'express';
-import { getAllOrders } from '../services/orderService';
+import express, { Response } from 'express';
+import { getAllOrders, getOrdersByName } from '../services/orderService';
 const router = express.Router();
+
+import { OrganizedOrders, OrderResponse, VaccineOrder as Order } from '../types'
+
+
+const sendOrders = (body: OrderResponse, res: Response) => {
+    if (body.orders.length === 0) res.sendStatus(404)
+    else res.send(body)
+}
+
+const organizeByProducer = (orders: Order[]) => {
+    const organizedOrders: OrganizedOrders = {
+        SolarBuddhica: [],
+        Zerpfy: [],
+        Antiqua: [],
+    }
+    orders.forEach(order => {
+        organizedOrders[order.vaccine].push(order)
+    })
+    return organizedOrders;
+}
 
 router.get('/', async (_req, res) => {
     try {
         const orders = await getAllOrders()
-        if (orders.length === 0) res.sendStatus(404)
-        else res.send(orders)
+        const organizedOrders = organizeByProducer(orders)
+        const body = {}
+        sendOrders(body, res)
     } catch (err) {
-        console.log(err);
+        throw new Error(err)
     }
 })
+
 
 export default router;
