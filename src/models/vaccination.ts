3952c3fa5c75c5118f29cbf2abcdbc6382852934
stuge <tuukkaveteli@gmail.com>
2021-07-28 15:09:04 +0300

    added bottleInfo virtual

--- src/models/vaccination.ts
+++ src/models/vaccination.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import {Gender, Vaccination} from '../types';
+import { Gender, Vaccination } from '../types';
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
@@ -11,11 +11,22 @@ const vaxSchema = new mongoose.Schema({
 	injected: { type: String, required: true }//date of injection
 });
 
+vaxSchema.virtual('bottleInfo', {
+	ref: 'Order',
+	localField: 'sourceBottle',
+	foreignField: 'orderId',
+	justOne: true,
+})
+
+vaxSchema.set('toObject', { virtuals: true });
 vaxSchema.set('toJSON', {
 	transform: (_document: unknown, returnedObj: Vaccination) => {
 		delete returnedObj._id;
 		delete returnedObj.__v;
 	},
+	virtuals: true
 });
+
+
 
 export default mongoose.model('Vaccination', vaxSchema);
