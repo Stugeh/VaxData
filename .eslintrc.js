d34dc5135d9a63cd862b8bd69ad4fa9f7720d18e
stuge <tuukkaveteli@gmail.com>
2021-07-28 15:44:58 +0300

    fixed eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,48 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true,
-		'node': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended'
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaFeatures': {
-			'jsx': true
-		},
-		'ecmaVersion': 12,
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'react',
-		'@typescript-eslint'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
-		'@typescript-eslint/semi': ['error'],
-		'@typescript-eslint/explicit-function-return-type': 0,
-		'@typescript-eslint/explicit-module-boundary-types': 0,
-		'@typescript-eslint/no-unsafe-member-access': 0,
-		'@typescript-eslint/no-explicit-any': 1,
-		'@typescript-eslint/no-unused-vars': [
-			'error', { 'argsIgnorePattern': '^_' }
-		],
-		'no-case-declarations': 0,
-		'react/prop-types': 0
-	}
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        '@typescript-eslint/semi': ['error'],
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        '@typescript-eslint/no-unsafe-member-access': 0,
+        '@typescript-eslint/no-explicit-any': 1,
+        '@typescript-eslint/no-unused-vars': [
+            'error', { 'argsIgnorePattern': '^_' }
+        ],
+        'no-case-declarations': 0,
+        'react/prop-types': 0
+    }
 };
