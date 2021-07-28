db6ba86683e3683bc68850ef65c6c44f90053d17
stuge <tuukkaveteli@gmail.com>
2021-07-28 12:54:18 +0300

    added tests for linesToOrder

--- __tests__/utils/migrate.ts
+++ __tests__/utils/migrate.ts
@@ -10,7 +10,10 @@ describe('Testing migrate scripts', () => {
         '/../data/Zerpfy.source',
     ];
     const firstOrderString = '{"id":"6da3a8cf-c923-4c77-8f80-c69c935fe1df","orderNumber":1,"responsiblePerson":"Joonatan Siloma","healthCareDistrict":"KYS","vaccine":"Antiqua","injections":4,"arrived":"2021-01-11T08:59:28.642790Z"}'
-
+    const invalidStringArray = [
+        '{"id":"6da3a8cf-c923-4c77-8f80-c69c935fe1df","orderNumber":1,"responsiblePerson":"Joonatan Siloma","healthCareDistrict":"KYS","vaccine":"Antiqua","injections":4,"arrived":"2021-01-11T08:59:28.642790Z"}',
+        '{"id":"6da3a8cf-c923-4c77-8f80-c69c935fe1df","orderNumber":1,"responsiblePerson":"Joonatan Siloma","healthCareDistrict":"KYS","vaccine":"Antiqua","injections":4,"arrived":"2021-01-11T08:59:28.642790Z'
+    ]
 
     it('Getting data from files works', () => {
         const data = migrate.getDataFromFiles(orderFiles)
@@ -18,8 +21,14 @@ describe('Testing migrate scripts', () => {
         expect(data[0]).toEqual(firstOrderString)
     })
 
-    it('', () => {
-
+    it('Order data is correctly converted to order objects', () => {
+        const data = migrate.getDataFromFiles(orderFiles).slice(0, 11)
+        const objects = migrate.linesToOrders(data)
+        expect(objects.length).toBe(data.length)
+        expect(objects[0].orderId).toBe("6da3a8cf-c923-4c77-8f80-c69c935fe1df")
+        expect(() => {
+            migrate.linesToOrders(invalidStringArray)
+        }).toThrow(SyntaxError)
     })
 
     it('', () => {
