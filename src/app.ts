69bee014c1e933c86e01e9fbe04419597e647b44
stuge <tuukkaveteli@gmail.com>
2021-07-28 16:02:03 +0300

    added route

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import {MONGOURL} from './config';
+
+import { MONGOURL } from './config';
+import orderRouter from './routes/orderRouter';
 
 const app = express();
-// models
-
-// utils
 
 // DB connection
 console.info('connecting to', MONGOURL);
 mongoose.connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .catch((err) => console.log('err', err));
+    .catch((err) => console.log('err', err));
 
 
 
@@ -20,6 +19,7 @@ app.use(express.static('build'));
 // middleware
 
 // Routers
+app.use('/api/orders/', orderRouter);
 
 
 export default app;
