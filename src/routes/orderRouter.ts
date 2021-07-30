ab98d3dc95221243b2f6b93908a9c644ca98baae
stuge <tuukkaveteli@gmail.com>
2021-07-30 10:11:56 +0300

    not checking whether there are orders returned

--- src/routes/orderRouter.ts
+++ src/routes/orderRouter.ts
@@ -1,25 +1,17 @@
-import express, { Response } from 'express';
+import express from 'express';
 import { getAllOrders, organizeByProducer } from '../services/orderService';
 const router = express.Router();
-
-import { OrderResponse } from '../types'
-
-
-const sendOrders = (body: OrderResponse, res: Response) => {
-    if (body.orders.length === 0) res.sendStatus(404)
-    else res.send(body)
-}
 
 
 router.get('/', async (_req, res) => {
     try {
-        const orders = await getAllOrders()
-        const organizedOrders = organizeByProducer(orders)
-        sendOrders(organizedOrders, res)
+        const orders = await getAllOrders();
+        const organizedOrders = organizeByProducer(orders);
+        res.send(organizedOrders);
     } catch (err) {
-        throw new Error(err)
+        throw new Error(err);
     }
-})
+});
 
 
 export default router;
