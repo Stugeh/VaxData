97fe79ed83116018fe26eb81675a4786f1bd0c24
stuge <tuukkaveteli@gmail.com>
2021-07-30 15:14:55 +0300

    wrote hook for fetching orders

---
+++ client/src/hooks/useFetchOrders.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { OrganizedOrders } from '../types';
+import validateData from '../utils/validateOrders';
+import { apiBaseUrl } from "../constants";
+
+
+const initData: OrganizedOrders = {
+    SolarBuddhica: [],
+    Zerpfy: [],
+    Antiqua: []
+};
+
+const useFetchOrders = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState(initData);
+    const [serverError, setServerError] = useState(null);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const fetchData = async () => {
+            try {
+                const resp = await axios.get<OrganizedOrders>(`${apiBaseUrl}/orders/`);
+                const data = validateData(resp.data);
+                setData(data);
+                setIsLoading(false);
+            } catch (error) {
+                setServerError(error);
+                setIsLoading(false);
+            }
+        };
+        void fetchData();
+    }, []);
+    
+    return {isLoading, data, serverError};
+};
+
+export default useFetchOrders;
