b437d957ac6bc7cd3e405f72ff01c87ceb6b6598
stuge <tuukkaveteli@gmail.com>
2021-07-30 12:16:09 +0300

    checking the structure of returned orders

--- __tests__/services/orderService.ts
+++ __tests__/services/orderService.ts
@@ -3,12 +3,24 @@ import mongoose from 'mongoose';
 import {MONGOURL} from '../../src/config';
 import {getAllOrders} from '../../src/services/orderService';
 
-describe('getAllOrders', () => {
+describe('Order service', () => {
     mongoose.connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true });
-    it('Can fetch orders', async () => {
+
+    it('getAll can fetch orders', async () => {
         const orders = await getAllOrders();
+        const order = orders[0];
+        const keys = [
+            'orderId', 'healthCareDistrict', 'orderNumber',
+            'responsiblePerson', 'injections', 'arrived',
+            'vaccine', 'vaccinations'
+        ];
         expect(orders.length).not.toBe(0);
+        keys.forEach(key =>
+            expect(order).toHaveProperty(key)
+        );
     });
+
+
     
     afterAll(async () => await mongoose.connection.close());
 });
