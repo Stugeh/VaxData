d05787f594217bb2f7476f95a624bd61fb4ff4d0
stuge <tuukkaveteli@gmail.com>
2021-07-30 15:36:28 +0300

    fetching orders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import useFetchOrders from './hooks/useFetchOrders';
 
 function App() {
+  const { data } = useFetchOrders();
+  console.log(`data.Antiqua[0]`, data);
   return (
     <div className="App">
-      
     </div>
   );
 }
