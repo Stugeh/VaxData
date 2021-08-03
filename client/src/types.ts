f4c4b411ed0c0e3151dfbb09cb2f1a0ad704b3dc
stuge <tuukkaveteli@gmail.com>
2021-08-03 18:12:09 +0300

    new types

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,9 +1,22 @@
 export type HealthCareDistrict = 'HYKS' | 'KYS' | 'OYS' | 'TAYS' | 'TYKS';
 
+export enum Gender {
+    'Male' = 'male',
+    'Female' = 'female',
+    'Other' = 'nonbinary'
+}
+
 export enum ProducerName {
     SolarBuddhica = 'SolarBuddhica',
     Zerpfy = 'Zerpfy',
     Antiqua = 'Antiqua'
+}
+
+export type Vaccination = {
+    vaccinationId: string;
+    gender: Gender;
+    sourceBottle: string;
+    injected: string;
 }
 
 export type Order = {
@@ -12,10 +25,20 @@ export type Order = {
     orderNumber: number,
     responsiblePerson: string,
     injections: number,
-    arrived: string,
+    arrived: Date,
     vaccine: ProducerName
+    vaccinations: Vaccination[]
 };
 
-export type OrganizedOrders = {
+export type Orders = {
     [key in ProducerName]: Order[]
 };
+
+export type UnknownOrders = {
+    [key in ProducerName]: unknown[]
+}
+
+export type DateAndOrders = {
+    orders: Orders,
+    date: Date | null
+}
