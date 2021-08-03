dc69d34e768a3776599397d428e9061b388f8c15
stuge <tuukkaveteli@gmail.com>
2021-08-03 13:53:00 +0300

    added date picker to top bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
-
+import { useState } from 'react';
 // import useFetchOrders from './hooks/useFetchOrders';
+
 import TopBar from './components/TopBar';
 
 function App() {
   // const { data } = useFetchOrders();
+  const [date, setDate] = useState<Date | null>(new Date());
   return (
     <div className="App">
-      <TopBar />
+      <TopBar setDate={setDate} date={date} />
     </div>
   );
 }
--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -1,14 +1,37 @@
+import { Dispatch, SetStateAction } from 'react';
 import {
-  AppBar, Toolbar, Typography, Button,
+  AppBar, Toolbar, Typography,
 } from '@material-ui/core';
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
-const TopBar = () => (
+const TopBar = ({ date, setDate }: {
+  date: Date|null, setDate: Dispatch<SetStateAction<Date|null>>
+}) => (
   <AppBar position="static">
     <Toolbar>
-      <Typography variant="h6">
-        News
+      <Typography variant="h4">
+        VaxData
       </Typography>
-      <Button color="inherit">Login</Button>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          className="date-picker"
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date picker inline"
+          value={date}
+          onChange={(newDate) => setDate(newDate)}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+      </MuiPickersUtilsProvider>
     </Toolbar>
   </AppBar>
 );
