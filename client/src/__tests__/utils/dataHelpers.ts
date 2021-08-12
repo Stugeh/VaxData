1625e0a4539110fed125139692400209dd818fa7
stuge <tuukkaveteli@gmail.com>
2021-08-12 14:17:47 +0300

    added tests for getOrdersToDate and on date

--- client/src/__tests__/utils/dataHelpers.ts
+++ client/src/__tests__/utils/dataHelpers.ts
@@ -1,21 +1,44 @@
 import {
-  isSameDay, isBefore, addDays, addSeconds, isAfter, max,
+  isSameDay, max, min,
 } from 'date-fns';
-import { testOrders } from '../../testConstants';
-import { getLatestDate } from '../../utils/dataHelpers';
+import { testOrders, spreadOrders } from '../../testConstants';
+import {
+  getLatestDate,
+  getOrdersToDate,
+  getOrdersOnDate,
+} from '../../utils/dataHelpers';
+
+const DATES = spreadOrders(testOrders)
+  .map((order) => order.arrived);
 
 describe('dataHelpers', () => {
   it('getLatestDate', () => {
-    const dates = [
-      ...testOrders.Antiqua,
-      ...testOrders.SolarBuddhica,
-      ...testOrders.Zerpfy,
-    ].map((order) => order.arrived);
     const latestDate = getLatestDate(testOrders);
-    expect(latestDate).toEqual(max(dates));
+    expect(latestDate).toEqual(max(DATES));
   });
 
-  it('', => {
-    
+  it('getOrdersToDate', () => {
+    const ordersBefore = getOrdersToDate({
+      orders: testOrders,
+      date: min(DATES),
+    });
+    const allOrders = getOrdersToDate({
+      orders: testOrders,
+      date: max(DATES),
+    });
+    const ordersBeforeArr = spreadOrders(ordersBefore);
+    const allOrdersArr = spreadOrders(allOrders);
+    expect(ordersBeforeArr.length).toBe(1);
+    expect(allOrdersArr.length).toBe(DATES.length);
+  });
+
+  it('ordersOnDate', () => {
+    const ordersOn = getOrdersOnDate({
+      orders: testOrders,
+      date: DATES[3],
+    });
+    const datesToday = DATES.filter((date) => isSameDay(date, DATES[3]));
+    const orderArr = spreadOrders(ordersOn);
+    expect(orderArr.length).toBe(datesToday.length);
   });
 });
