d78453647570cd0326bd8da5486149d904209ee2
stuge <tuukkaveteli@gmail.com>
2021-08-12 12:31:30 +0300

    simplified getLatestDate

--- client/src/utils/dataHelpers.ts
+++ client/src/utils/dataHelpers.ts
@@ -1,27 +1,22 @@
 /* eslint-disable import/prefer-default-export */
 import {
-  isSameDay, isBefore, addDays, addSeconds, isAfter,
+  isSameDay, isBefore, addDays, addSeconds, isAfter, max,
 } from 'date-fns';
 import {
-  Counts, DateAndOrders, Orders, ProducerName,
+  Counts, DateAndOrders, Orders,
   Order, Vaccination, LooseObjectObject,
 } from '../types';
 
 // gets the date of the latest arrival
 export const getLatestDate = (data: Orders): Date => {
-  // if we don't have data return current date
   if (data.SolarBuddhica.length === 0) return new Date();
-  // get all the producer names from the enum so we can loop
-  const keys: (keyof typeof ProducerName)[] = Object
-    .values(ProducerName);
-  // get the latest arrival date of all producers
-  // and return them as date objects. (the data is sorted on the backend)
-  const firstDates = keys.map((key) => new Date(
-    data[key][0].arrived,
-  ));
-  // sort the combined array and return the most recent date.
-  firstDates.sort((a, b) => b.getTime() - a.getTime());
-  return firstDates[0];
+  const allOrders = [
+    ...data.SolarBuddhica,
+    ...data.Antiqua,
+    ...data.Zerpfy,
+  ];
+  const dates = allOrders.map((order) => order.arrived);
+  return max(dates);
 };
 
 // gets orders before given date from an array
