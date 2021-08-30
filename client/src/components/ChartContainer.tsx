28bd7984add0996428ed12e436b77633e4906892
stuge <tuukkaveteli@gmail.com>
2021-08-30 10:57:52 +0300

    added a spinner

--- client/src/components/ChartContainer.tsx
+++ client/src/components/ChartContainer.tsx
@@ -2,7 +2,7 @@ import { orderCountsToBarChart, vaccineCountsToChart } from '../utils/chartForma
 import { DataOutput } from '../types';
 import BarChart from './BarChart';
 
-const ChartContainer = ({ data }: {data: DataOutput}) => {
+const ChartContainer = ({ data, loading }: {data: DataOutput, loading: boolean}) => {
   const { cumulativeCounts, countsOnDate } = data;
   return (
     <div className="chart-container">
@@ -10,30 +10,35 @@ const ChartContainer = ({ data }: {data: DataOutput}) => {
         header="Cumulative Orders"
         data={orderCountsToBarChart(cumulativeCounts)}
         keys={['available', 'consumed', 'expired']}
+        loading={loading}
         index="producer"
       />
       <BarChart
         header="Cumulative doses"
         data={vaccineCountsToChart(cumulativeCounts)}
         keys={['available', 'used', 'expired']}
+        loading={loading}
         index="producer"
       />
       <BarChart
         header="Orders Today"
         data={orderCountsToBarChart(countsOnDate)}
         keys={['arrived', 'expired']}
+        loading={loading}
         index="producer"
       />
       <BarChart
         header="Doses Today"
         data={vaccineCountsToChart(countsOnDate)}
         keys={['used', 'expired', 'arrived']}
+        loading={loading}
         index="producer"
       />
       <BarChart
         header="Doses expiring within 10 days"
         data={vaccineCountsToChart(cumulativeCounts)}
         keys={['expiring']}
+        loading={loading}
         index="producer"
       />
       <div className="chartFiller" />
